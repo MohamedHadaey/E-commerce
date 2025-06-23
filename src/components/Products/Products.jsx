533ddefd085c18985909040a54ae6c2306f21e9d
mohamed hadaey <devmohamedhadaey@gmail.com>
2025-06-23 21:51:38 +0300

    Implement product listing with API integration and styling
    Added product fetching from API and display logic in Products.jsx, including a loading spinner and product card layout. Updated index.css with styles for product cards. Simplified ProtectedRoute to check token from localStorage instead of context. Removed StrictMode wrapper in main.jsx.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,7 +1,57 @@
-﻿import React from 'react'
+﻿import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { FallingLines } from 'react-loader-spinner';
 
 export default function Products() {
-  return (
-    <div>Products</div>
-  )
+  const [allProducts, setAllProducts] = useState(null)
+
+  async function getAllProducts() {
+    const response = await axios.get('https://ecommerce.routemisr.com/api/v1/products');
+    console.log('response 123', response)
+    setAllProducts(response.data.data);
+  }
+
+  useEffect(() => {
+    getAllProducts();
+  }, [])
+
+  return <>
+    <div id="products">
+      <div className="title text-center mt-5 mb-10">
+        <h1 className='text-3xl font-bold text-gray-500'>Products</h1>
+      </div>
+      {allProducts == null ? <div className="spinner flex justify-center items-center w-full my-30">
+        <FallingLines
+          color="#4fa94d"
+          width="100"
+          visible={true}
+          ariaLabel="falling-circles-loading"
+        />
+      </div> : <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-3">
+        {allProducts.map((product) => {
+          return (
+            <div className="product" key={product._id}>
+              <div className="product-image">
+                <img src={product.imageCover} alt={product.title} className='w-full ' />
+              </div>
+              <div className="product-info">
+                <h6 className='product-category'>{product.category.name}</h6>
+                <h3 className="product-title">{product.title}</h3>
+                <div className='flex justify-between items-center '>
+                  <div className="price">
+                    <p>{product.price} EGP</p>
+                  </div>
+                  <div className="rate">
+                    <span>
+                      <i className="fa-solid fa-star"></i> {product.ratingsAverage}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>}
+    </div>
+  </>
 }
--- src/components/Protected/ProtectedRoute.jsx
+++ src/components/Protected/ProtectedRoute.jsx
@@ -1,10 +1,7 @@
 ﻿import { Navigate } from "react-router-dom";
-import { AuthContext } from "../../Context/AuthContext";
-import { useContext } from "react";
 
 export default function ProtectedRoute({children}) {
-    const { token } = useContext(AuthContext)
-    if (token == null) {
+    if (localStorage.getItem('token') == null) {
         return <Navigate to='/login' />
     } else {
         return <>
