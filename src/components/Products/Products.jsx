e4706df978e3ee4350aaa747ccd0a752d617fd75
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2025-06-25 12:49:07 +0300

    Refactor Products to use react-query for data fetching
    Replaces useEffect and useState with react-query's useQuery for fetching products. Adds loading and error states using react-query, improving data management and UI feedback.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,66 +1,90 @@
 ﻿import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { FallingLines } from 'react-loader-spinner';
+import { useQuery } from 'react-query';
 
 export default function Products() {
-  const [allProducts, setAllProducts] = useState(null)
-
-  async function getAllProducts() {
-    const response = await axios.get('https://ecommerce.routemisr.com/api/v1/products');
-    console.log('response 123', response)
-    setAllProducts(response.data.data);
+  function getAllProducts() {
+    return axios.get('https://ecommerce.routemisr.com/api/v1/products');
   }
 
-  useEffect(() => {
-    getAllProducts();
-  }, [])
+  const { data, isError, isLoading, error } = useQuery({
+    queryKey: ['allProducts'],
+    queryFn: getAllProducts,
+  })
 
-  return <>
-    <div id="products" className='container mx-auto'>
-      <div className="title text-center mt-5 mb-10">
-        <h1 className='text-3xl font-bold text-gray-500'>Products</h1>
+  if (isLoading) {
+    return <>
+      <div id="products" className='container mx-auto'>
+        <div className="title text-center mt-5 mb-10">
+          <h1 className='text-3xl font-bold text-gray-500'>Products</h1>
+        </div>
+        <div className="spinner flex justify-center items-center w-full my-30">
+          <FallingLines
+            color="#4fa94d"
+            width="100"
+            visible={true}
+            ariaLabel="falling-circles-loading"
+          />
+        </div>
       </div>
-      {allProducts == null ? <div className="spinner flex justify-center items-center w-full my-30">
-        <FallingLines
-          color="#4fa94d"
-          width="100"
-          visible={true}
-          ariaLabel="falling-circles-loading"
-        />
-      </div> : <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-3">
-        {allProducts.map((product) => {
-          return (
-            <div className="product" key={product._id}>
-              <div className="product-image">
-                <img src={product.imageCover} alt={product.title} className='w-full ' />
-              </div>
-              <div className="product-info">
-                <h6 className='product-category'>{product.category.name}</h6>
-                <h3 className="product-title">{product.title.split(' ').slice(0,3).join(' ')}</h3>
-                <div className='flex justify-between items-center '>
-                  <div className="price">
-                    <p>
-                      <span className={product.priceAfterDiscount ? 'line-through text-red-600' : ''}>
-                        {product.price}
+    </>
+  }
+
+  if (isError) {
+    return <>
+      <div id="products" className='container mx-auto'>
+        <div className="title text-center mt-5 mb-10">
+          <h1 className='text-3xl font-bold text-gray-500'>Products</h1>
+        </div>
+        <div className="spinner flex justify-center items-center w-full my-30">
+          <p>fe error = {error.error.message}</p>
+        </div>
+      </div>
+    </>
+  }
+
+  if (data) {
+    return <>
+      <div id="products" className='container mx-auto'>
+        <div className="title text-center mt-5 mb-10">
+          <h1 className='text-3xl font-bold text-gray-500'>Products</h1>
+        </div>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-3">
+          {data.data.data.map((product) => {
+            return (
+              <div className="product" key={product._id}>
+                <div className="product-image">
+                  <img src={product.imageCover} alt={product.title} className='w-full ' />
+                </div>
+                <div className="product-info">
+                  <h6 className='product-category'>{product.category.name}</h6>
+                  <h3 className="product-title">{product.title.split(' ').slice(0, 3).join(' ')}</h3>
+                  <div className='flex justify-between items-center '>
+                    <div className="price">
+                      <p>
+                        <span className={product.priceAfterDiscount ? 'line-through text-red-600' : ''}>
+                          {product.price}
+                        </span>
+                        {
+                          product.priceAfterDiscount && (<span>
+                            {product.priceAfterDiscount}
+                          </span>)
+                        }
+                        EGP</p>
+                    </div>
+                    <div className="rate">
+                      <span>
+                        <i className="fa-solid fa-star"></i> {product.ratingsAverage}
                       </span>
-                      {
-                        product.priceAfterDiscount && (<span>
-                          {product.priceAfterDiscount}
-                        </span>)
-                      }
-                      EGP</p>
-                  </div>
-                  <div className="rate">
-                    <span>
-                      <i className="fa-solid fa-star"></i> {product.ratingsAverage}
-                    </span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          )
-        })}
-      </div>}
-    </div>
-  </>
+            )
+          })}
+        </div>
+      </div>
+    </>
+  }
 }
