634e1d4a5f9216b5a8f7e4f4f6db7a95c44858bf
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2025-06-25 15:30:29 +0300

    Refactor CategoriesSliders to use react-query
    Replaces useEffect and useState with react-query for fetching categories. Improves data fetching logic, adds loading and error states, and simplifies component structure.

--- src/components/CategoriesSliders/CategoriesSliders.jsx
+++ src/components/CategoriesSliders/CategoriesSliders.jsx
@@ -1,5 +1,6 @@
 ﻿import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useQuery } from "react-query";
 import Slider from "react-slick";
 
 function CategoriesSliders() {
@@ -23,30 +24,34 @@ function CategoriesSliders() {
         waitForAnimate: false,
     };
 
-    const [allCategories, setAllCategories] = useState(null)
-
-    async function getAllCategories() {
-        axios.get('https://ecommerce.routemisr.com/api/v1/categories')
-            .then((response) => {
-                console.log('response123', response.data.data);
-                const categories = response.data.data;
-                const first8Categories = categories.slice(0, 8);
-                setAllCategories(first8Categories);
-            })
-            .catch((error) => {
-                console.log('error', error)
-            })
+    function getAllCategories() {
+        return axios.get('https://ecommerce.routemisr.com/api/v1/categories')
     }
 
-    useEffect(() => {
-        getAllCategories();
-    }, [])
+    const { data, isLoading, isError, error } = new useQuery({
+        queryKey: ['allCategories'],
+        queryFn: getAllCategories,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+    });
 
-    return <>
-        {allCategories === null ? <p>Loading...</p> :
+    if (isLoading) {
+        return <>
+            <p>Loading...</p>
+        </>
+    }
+
+    if (isError) {
+        return <>
+            <p>{error.error.message}</p>
+        </>
+    }
+
+    if (data) {
+        return <>
             <div className="slider-container">
                 <Slider {...settings}>
-                    {allCategories.map((category) => {
+                    {data.data.data.slice(0, 8).map((category) => {
                         return (
                             <div className="category-slider" key={category._id}>
                                 <img src={category.image} alt={category.name} className="w-full" loading="lazy" />
@@ -56,8 +61,8 @@ function CategoriesSliders() {
                     })}
                 </Slider>
             </div>
-        }
-    </>
+        </>
+    }
 }
 
 export default CategoriesSliders;
