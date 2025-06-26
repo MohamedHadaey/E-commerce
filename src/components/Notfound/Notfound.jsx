9a416c4dcefc18aa95529fc70ebd1d7feb0c5b56
mohamed hadaey <105833278+MohamedHadaey@users.noreply.github.com>
2025-06-26 18:32:38 +0300

    Improve Notfound image styling and layout
    Removed fixed height from the container and added inline styles to the image for a maximum width of 600px and centered alignment. This enhances the appearance and responsiveness of the Notfound component.

--- src/components/Notfound/Notfound.jsx
+++ src/components/Notfound/Notfound.jsx
@@ -4,8 +4,8 @@ import notFoundImg from '../../assets/images/notfound.svg';
 export default function Notfound() {
   return <>
     <div id="notfound">
-      <div className="container mx-auto h-50 px-5 py-10">
-          <img src={notFoundImg} alt="not found image" className='w-full'/>
+      <div className="container mx-auto  px-5 py-10">
+          <img src={notFoundImg} alt="not found image" className='w-full' style={{maxWidth: '600px', margin: '0 auto'}}/>
       </div>
     </div>
   </>
